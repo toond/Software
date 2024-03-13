a6dec144bb867ba6addffa996a36db5c4b0dce11
toond <078528@naver.com>
2024-03-13 15:41:25 +0900

    commit

---
+++ hello2.java
@@ -0,0 +1,12 @@
+public class hello2 {
+    public static void main(String[] args) {
+
+        String str1 = "Hello";
+        String str2;
+
+
+        str2 = str1.toUpperCase();
+
+        System.out.println(str2);
+    }
+}
